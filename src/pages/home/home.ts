ecca6cf44a69fd662a4141d654ebd9bdb4aec59c
asoilihi <lhabdou26@hotmail.fr>
2018-09-07 17:24:42 +0200

    aso - home config 2nd page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,4 @@
+import { AppareilsPage } from './../appareils/appareils';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
@@ -7,8 +8,10 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController) { }
 
+  onGoToAppareils() {
+    this.navCtrl.push(AppareilsPage);
   }
 
 }
