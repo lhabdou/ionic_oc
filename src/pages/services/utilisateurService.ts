7053ddd97b5106c7d80bf1c3816a3b9c40b44274
asoilihi <lhabdou26@hotmail.fr>
2018-10-04 17:23:51 +0200

    aso - get user profil

--- src/pages/services/utilisateurService.ts
+++ src/pages/services/utilisateurService.ts
@@ -1,11 +1,10 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { IUtilisateur } from '../modeles/utilisateurModel';
-import { AngularFireAuth } from 'angularfire2/auth';
-import { ENVIRONNEMENT } from '../../constantes/constantesUtilis';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { IUtilisateur } from "../modeles/utilisateurModel";
+import { AngularFireAuth } from "angularfire2/auth";
+import { ENVIRONNEMENT } from "../../constantes/constantesUtilis";
 @Injectable()
 export class UtilisateurService {
-
   user: IUtilisateur = {
     idUtilisateur: "",
     nom: "defaultName",
@@ -19,56 +18,72 @@ export class UtilisateurService {
     mdp: "123456",
     pseudo: "lhabdou",
     email: "lhabdou26@hotmail.fr"
-  }
+  };
 
-  constructor(private afAuth: AngularFireAuth, private httpClient: HttpClient) {
-
-  }
+  constructor(
+    private afAuth: AngularFireAuth,
+    private httpClient: HttpClient
+  ) {}
 
   getUserConnected(): IUtilisateur {
-
     if (this.afAuth.auth.currentUser) {
-
       return this.getUserProfile(this.afAuth.auth.currentUser.email);
       // this.user.nom = "Soilihi";
       // this.user.email = this.afAuth.auth.currentUser.email;
       // this.user.prenom = "Abdoul";
-
     }
   }
 
-  getUserProfile(email:string):IUtilisateur {
-
+  getUserProfile(email: string): IUtilisateur {
     const httpOptions = {
       headers: new HttpHeaders({
-        'Content-Type':  'application/json',
-        'token': this.user.token
-        //'Authorization': 'my-auth-token'
+        "Content-Type": "application/json",
+        token: this.user.token
       })
     };
 
-    return this.user;
+    this.httpClient.get(
+      ENVIRONNEMENT.URL_REST_LOCAL + "/utilisateurs/"+ this.user.idUtilisateur,
+      httpOptions
+    )
+    .subscribe(
+      data => {},
+      error => {
+        console.log(
+          "Erreur lors de l'enregistrement d'un utilisateur",
+          error
+        );
+        console.log("mak")
+      }
+    ).closed;
 
+
+    return this.user;
   }
 
   saveProfileUser(user: IUtilisateur) {
-
-
     const httpOptions = {
       headers: new HttpHeaders({
-        'Content-Type':  'application/json',
-        'token': user.token
-        //'Authorization': 'my-auth-token'
+        "Content-Type": "application/json",
+        "Accept-Type": "application/json",
+        token: user.token
       })
     };
 
-
-    this.httpClient.post(ENVIRONNEMENT.URL_REST_LOCAL + "/utilisateurs/nouveau", user, httpOptions)
-      .subscribe(data => {
-
-      }, error => {
-        console.log("Erreur lors de l'enregistrement d'un utilisateur",error);
-      });
+    this.httpClient
+      .post(
+        ENVIRONNEMENT.URL_REST_LOCAL + "/utilisateurs/nouveau",
+        user,
+        httpOptions
+      )
+      .subscribe(
+        data => {},
+        error => {
+          console.log(
+            "Erreur lors de l'enregistrement d'un utilisateur",
+            error
+          );
+        }
+      );
   }
-
 }
